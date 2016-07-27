111a37a7fcfa16d34bf04891821ea6e07af1b0bd
Qexy.org <admin@qexy.org>
2016-07-27 22:07:44 +0400

    First upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# WebMCR-Reloaded-Bootstrap-4
 Bootstrap 4 Theme for WebMCR Reloaded
+
+Upload folder "bootstrap_4" inside "themes" folder and change template settings in control panel
---
+++ bootstrap_4/theme.php
@@ -0,0 +1,13 @@
+<?php
+
+if(!defined("MCR")){ exit("Hacking Attempt!"); }
+
+$theme = array(
+	"ThemeName" => "Bootstrap 4",
+	"Author" => "Toster_tpl",
+	"AuthorUrl" => "http://webmcr.com",
+	"About" => "Шаблон на основе фреймворка Bootstrap 4 Alpha 2",
+	"Version" => "1.0 [WebMCR Reloaded 1.3.9]"
+);
+
+?>
